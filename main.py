37bb83c6a7f490dfcc44db660ee84a2dba97ecfd
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-25 03:18:51 +0000

    finished project

--- main.py
+++ main.py
@@ -4,16 +4,38 @@ from art import logo
 from random import randint
 
 def play_game():
-
   answer = randint(1, 100)
   
+  player_turns = 5
   print(logo)
   print("Welcome to Guess A Number! The random number guessing game.")
   print("I'm thinking of a number between 1 and 100.")
   print(f"The correct number is {answer}")
-  input("Choose a difficulty. Type 'easy' or 'hard': ")
+  level = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
+  if level == "easy":
+    player_turns = 10
+
+  while player_turns != 0:
+    print(f"You have {player_turns} chances left.")
+    guess = int(input("Take a guess: "))
+
+    if guess == answer:
+      print("Correct You Win!")
+    elif guess < answer:
+      player_turns -= 1
+      print("Too Low")
+      print("Guess Again.")
+    elif guess > answer:
+      player_turns -= 1
+      print("Too High")
+      print("Guess Again.")
+    else:
+      player_turns -= 1
+      print("Guess Again.")
   
+
+play_game()  
 # Allow the player to submit a guess for a number between 1 and 100.
 # Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
 # If they got the answer correct, show the actual answer to the player.
